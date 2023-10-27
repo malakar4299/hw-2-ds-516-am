ea819a2b63049bc2fb040bb26b2cebe918e1536b
Abhishek <amalakar@bu.edu>
2023-10-26 21:59:45 -0400

    SQL changes

--- hw-4/main.py
+++ hw-4/main.py
@@ -6,6 +6,14 @@ import functions_framework
 from flask_cors import cross_origin
 from google.cloud import pubsub_v1
 import json
+import datetime
+
+import sqlalchemy
+
+from connect_connector import connect_with_connector
+from connect_connector_auto_iam_authn import connect_with_connector_auto_iam_authn
+from connect_tcp import connect_tcp_socket
+from connect_unix import connect_unix_socket
 
 app = Flask(__name__)
 
@@ -21,6 +29,63 @@ logger = logging_client.logger('gcs-file-requests')
 BANNED_COUNTRIES = ['North Korea', 'Iran', 'Cuba', 'Myanmar', 'Iraq', 'Libya', 'Sudan', 'Zimbabwe', 'Syria']
 SECOND_APP_URL = "http://localhost:5001/alert"
 HTTP_METHODS = ['GET','POST','PUT', 'DELETE', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+
+
+def init_connection_pool() -> sqlalchemy.engine.base.Engine:
+    """Sets up connection pool for the app."""
+    # use a TCP socket when INSTANCE_HOST (e.g. 127.0.0.1) is defined
+    if os.environ.get("INSTANCE_HOST"):
+        return connect_tcp_socket()
+
+    # use a Unix socket when INSTANCE_UNIX_SOCKET (e.g. /cloudsql/project:region:instance) is defined
+    if os.environ.get("INSTANCE_UNIX_SOCKET"):
+        return connect_unix_socket()
+
+    # use the connector when INSTANCE_CONNECTION_NAME (e.g. project:region:instance) is defined
+    if os.environ.get("INSTANCE_CONNECTION_NAME"):
+        # Either a DB_USER or a DB_IAM_USER should be defined. If both are
+        # defined, DB_IAM_USER takes precedence.
+        return (
+            connect_with_connector_auto_iam_authn()
+            if os.environ.get("DB_IAM_USER")
+            else connect_with_connector()
+        )
+
+    raise ValueError(
+        "Missing database connection type. Please define one of INSTANCE_HOST, INSTANCE_UNIX_SOCKET, or INSTANCE_CONNECTION_NAME"
+    )
+
+
+# create 'votes' table in database if it does not already exist
+def migrate_db(db: sqlalchemy.engine.base.Engine) -> None:
+    """Creates the `votes` table if it doesn't exist."""
+    with db.connect() as conn:
+        conn.execute(
+            sqlalchemy.text(
+                "CREATE TABLE IF NOT EXISTS votes "
+                "( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, "
+                "candidate VARCHAR(6) NOT NULL, PRIMARY KEY (vote_id) );"
+            )
+        )
+        conn.commit()
+
+
+# This global variable is declared with a value of `None`, instead of calling
+# `init_db()` immediately, to simplify testing. In general, it
+# is safe to initialize your database connection pool when your script starts
+# -- there is no need to wait for the first request.
+db = None
+
+
+# init_db lazily instantiates a database connection pool. Users of Cloud Run or
+# App Engine may wish to skip this lazy instantiation and connect as soon
+# as the function is loaded. This is primarily to help testing.
+@app.before_first_request
+def init_db() -> sqlalchemy.engine.base.Engine:
+    """Initiates connection to database and its' structure."""
+    global db
+    db = init_connection_pool()
+    migrate_db(db)
 
 
 @cross_origin()
